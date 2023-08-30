907b4db4f8d8b95535354fbd3705ab9698dd3f6e
Gbolahan2003 <matthewarowosegbe4@gmail.com>
2023-08-30 15:31:47 +0100

    'third-commit'

--- src/app/About-page/components/About.tsx
+++ src/app/About-page/components/About.tsx
@@ -1,10 +1,16 @@
+'use client'
+
+
 import React from 'react'
 import Image from 'next/image'
 import '../../../Styles/About.css'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import Picture from '../../../../public/images/png/About1.png'
 import Picture1 from '../../../../public/images/png/About2.png'
 import Picture2 from '../../../../public/images/png/About3.png'
+ 
 const About = () => {
   return (
     <div>
@@ -27,15 +33,24 @@ const About = () => {
                     </div>
                 </div>
                 <div className="about-button">
-                <button>
-                    meet the Team
+                <button onClick={()=> useRouter().push('/About-page')} >
+                   meet the Team
                 </button>
             </div>
            </div> 
            <div className="about-us-image_container">
-            <div className="pic-1 pi"><Image src={Picture1} alt='leone'/></div>
+            <div className="pic-1 pi">
+            <svg xmlns="http://www.w3.org/2000/svg" width="138" height="111" viewBox="0 0 138 111" fill="none">
+  <circle cx="69" cy="86" r="25" fill="#092643"/>
+  <circle cx="48" cy="20" r="15" fill="#092643"/>
+  <circle cx="128" cy="92" r="10" fill="#092643"/>
+  <circle cx="10" cy="10" r="10" fill="#092643"/>
+</svg>
+            </div>
             <div className="picture"><Image src={Picture} alt='leone'/></div>
-            <div className="pic-1 pic"><Image src={Picture2} alt='leone'/></div>
+            <div className="pic-1 pic"><svg xmlns="http://www.w3.org/2000/svg" width="100" height="95" viewBox="0 0 100 95" fill="none">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M51.284 0.106649C62.1768 -0.835356 72.8905 4.57632 81.0449 11.8596C88.7171 18.7121 91.5175 28.934 94.5611 38.7604C97.5595 48.4406 102.518 58.8618 98.4828 68.1577C94.5133 77.3026 83.2576 79.9887 74.5546 84.8511C67.0368 89.0513 59.8952 94.6018 51.284 94.5278C42.7102 94.4541 35.9754 88.2796 28.3023 84.4535C18.6546 79.6429 4.13196 79.4765 0.642555 69.2763C-2.83582 59.1084 8.73034 50.2132 13.2543 40.4653C17.0698 32.2439 18.826 23.2992 25.0419 16.7027C32.3178 8.98116 40.714 1.02075 51.284 0.106649Z" fill="#092643"/>
+</svg></div>
             </div>
             
             </section>  
--- src/app/get-involved/page.tsx
+++ src/app/get-involved/page.tsx
@@ -1,5 +1,9 @@
+import Team from "../About-page/components/Team";
+
 export default function GetInvolved(){
     return (
-        <></>
+        <>
+      
+        </>
     )
 }
